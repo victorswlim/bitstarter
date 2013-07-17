0bacd940b85bfa4a0c459ecc4d5c1cc94a142df9
Victor Lim <victorswlim@yahoo.com>
2013-07-17 16:59:43 -0700

    Update web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 
-  out = fs.readFilesync(index.thml);
+  out = fs.readFilesync(index.html);
   response.send('Again');
 });
 
